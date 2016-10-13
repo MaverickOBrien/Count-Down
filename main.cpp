004e8a014a1ec39d64bde537a667fbe8109c9a98
Maverick OBrien <maverickobrien@github.com>
2016-10-13 10:35:57 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    int Seconds;
+    time_t t = time(0);
+
+    cout << "Welcome to the KoolKids timer" <<endl <<endl;
+    cout << "Imput the amount of seconds you want to count down:" <<endl;
+    cin >> Seconds;
+    cout << "Starting count down from " << Seconds <<endl;
+    for (int i = Seconds -1; i >= 0; i--)
+    cout << "Time remaining: " << i <<endl;
+    cout <<endl;
+    cout << "KAAAAABOOOOOOOOOM, I R DUN!" <<endl;
+    cout <<endl;
+    cout << "Current Date/Time:" <<endl;
+    struct tm * now =localtime( & t );
+    cout << (now->tm_year +1900) << '-'
+        << (now->tm_mon + 1) << '-'
+        << now->tm_mday << '/'
+        << now->tm_hour - 12 << ':'
+        << now->tm_min << ':'
+        << now->tm_sec <<endl;
+    return 0;
+}
